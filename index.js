6300b0919c75b968fb0d2f2f841e98e190061f7f
samcooper7 <samuelcooper663@gmail.com>
2025-11-28 13:50:27 +0000

    app is complete fully functional

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const numberOfDice = 3;
 const diceContainer = document.querySelector('.dice-container');
 const btnRollDice = document.querySelector('.btn-roll-dice');
+const totalDisplay = document.querySelector('.dice-number');
+const successAlert = document.getElementById('success-alert');
 
 
 function createDice(number) {
@@ -47,7 +49,7 @@ function createDice(number) {
 
     for (const dotPosition of dotPositionMatrix[number]) {
         const dot = document.createElement('div');
-        dot.classList.add('dot');
+        dot.classList.add('dice-dot');
         dot.style.setProperty('--left', dotPosition[0] + '%');
         dot.style.setProperty('--top', dotPosition[1] + '%');
         dice.appendChild(dot);
@@ -59,24 +61,51 @@ function createDice(number) {
 }
 
 function randomiseDice(diceContainer, numberOfDice) {
+    const rolls = [];
     diceContainer.innerHTML = '';
     for (let i = 0; i < numberOfDice; i++) {
         const randomNumber = Math.floor(Math.random() * 6) + 1;
         const dice = createDice(randomNumber);
         diceContainer.appendChild(dice);
+        rolls.push(randomNumber);
     }
+    return rolls;
 }
 
+function countDiceNumber(rolls) {
+    let total = 0;
+    for (let i = 0; i < rolls.length; i++) {
+        total += rolls[i];
+    } 
+    if (total >= 10) {
+        successAlert.style.display = 'block';
+    } else {
+        successAlert.style.display = 'none';
+    }
+    totalDisplay.textContent = `${total}`;
+}
 
-randomiseDice(diceContainer, numberOfDice);
+const initialRolls = randomiseDice(diceContainer, numberOfDice);
+countDiceNumber(initialRolls);
 
 btnRollDice.addEventListener('click', () => {
+
+    successAlert.style.display = 'none';
+    totalDisplay.textContent = '';
+
+
     const interval = setInterval(() => {
         randomiseDice(diceContainer, numberOfDice);
     }, 50);
 
     setTimeout(() => {
         clearInterval(interval);
+
+
+        const finalRolls = randomiseDice(diceContainer, numberOfDice);
+        countDiceNumber(finalRolls);
+
+
     }, 1000);
 });
 
